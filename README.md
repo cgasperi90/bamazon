b180a4039603194830f69105da0f1976b7ba88fa
Carlos Gasperi <cgasperi90@gmail.com>
2020-04-20 11:03:06 -0400

    Added functionality to the app. The app can now run completely and function without any issues. The user can make a purchase for any item nested within the database.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # bamazon
+
+### The purpose
+This application is intended for a large variety of audience. Anyone that has access to the application can enter the app and place an order for an item that is for sale in the store. The application is geared towards businesses that want to sell a variety of products on the internet. This app is made for businesses to be able to publish their products in order to reach a larger audience and generate more sales as well.
+
+### Instructions
+Upon entering the application, the user can choose to make a purchase or exit the program. If the user chooses to make a purchase, then the items that are available will populate onto the screen, at this time the user can make a selection on which item they would like and how many of those items they would like. Once everything is chosen, then the user will see a summary of their purchase along with a total for their order.
+
+### Technologies
+In bamazon, I used multiple technologies in order to achieve success in creating this app.
+*   MySQL
+*   Node.js
+    ###### Dependencies
+    *   Inquirer NPM PAckage
+    *   MYSQL NPM Package
+*   Javascript
+
+## Organization
+This program contains all of its code and is ran from one single Javascript file. Along with the single Javacript file, the program gets its information from the MYSQL database that contains all of the items available for sale in the app.
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,125 @@
+
+//Here I have my require variables for the NPM packages.
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+//Here is where I establish my connection to MYSQL.
 var connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     user: "root",
-    password: "3730281z"
+    password: "3730281z",
+    database: "bamazon"
 });
 
+//This how we connect to MYSQL and start the program.
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected!\n");
+    showItems();
   });
+
+//Here we have a function that asks the user if they would like to make a purchase or exit the program
+//If they choose to make a purchase, then the function populates the items in the database and asks which one the user would like.
+function showItems() {
+    inquirer.prompt([
+        {
+            name: "enter",
+            type: "list",
+            message: "Would you like to make a purchase or exit?",
+            choices: ["PURCHASE", "EXIT"]
+        }
+
+    ]).then(function(answer) {
+
+        if (answer.enter === "PURCHASE") {
+
+            console.log("---------------------------------------------------");
+            console.log("Which item would you like to purchase?");
+            console.log("---------------------------------------------------");
+
+            //Here we select all the items from the database and display them to the console.
+            connection.query("SELECT * FROM products", function(err, results) {
+            if (err) throw err;
+            for (var i = 0; i < results.length; i++) {
+                
+                console.log("\tItem Number: " + results[i].item_id + 
+                " | " + "Item: " + results[i].product_name + 
+                " | " + "Department: " + results[i].department_name + 
+                " | " + "Price: $" + results[i].price + "\n");
+            }
+            //The buy items function gets ran here so the user can make a selection.
+            buyItems();
+            });
+            } else {
+                console.log("---------------------------------------------------");
+                console.log("Thanks for visiting!");
+                console.log("---------------------------------------------------");
+                connection.end();
+            }
+    });
+}
+
+//This function asks the user to type in the ID of the item they would like to purchase and also checks to see
+//if there is enough stock to fulfill the user's order.
+function buyItems() {
+
+    //Inquirer asks the user to type in the ID of the item and to type in the quantity.
+    inquirer.prompt([
+        {
+            name: "itemId",
+            type: "input",
+            message: "Type the ID number of the item you would like to purchase."
+        },
+        {
+            name: "stock",
+            type: "input",
+            message: "How many of these items would you like?"
+        }
+    ]).then(function(answer) {
+
+        connection.query("SELECT * FROM products", function(err, results) {
+            if (err) throw err;
+
+            //Here I created a variable that will hold the selection of the user.
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id === parseInt(answer.itemId)) {
+                    chosenItem = results[i];
+                
+                }
+            }
+
+            
+            //This if statement compares the stock quantity in the database to the stock quantity that the user has chosen to purchase,
+            //then the stock quantity in the database gets updated with the new amoount.
+            //If the user chooses an amount higher than what is available, then the order will not get fullfilled.
+            if (chosenItem.stock_quantity > answer.stock) {
+                connection.query("UPDATE products SET ? WHERE ?", 
+                [
+                    {
+                        stock_quantity: (chosenItem.stock_quantity - parseInt(answer.stock))
+                    },
+                    {
+                        item_id: chosenItem.item_id
+                    }
+                ],
+                function(err, res) {
+                    if (err) throw err;
+
+                    //Here the user gets a summary of what they have purchased and their total for the entire order.
+                    console.log("---------------------------------------------------");
+                    console.log("Congrats! You chose to purchase " + chosenItem.product_name +  "!");
+                    console.log("---------------------------------------------------");
+                    console.log("Your Order Total is $" + parseInt(answer.stock) * chosenItem.price + "\nThank you for your Purchase!");
+                    console.log("---------------------------------------------------");
+                    showItems();
+                })
+            } else {
+                console.log("---------------------------------------------------");
+                console.log("Sorry, we don't have that many " + chosenItem.product_name + " in stock.");
+                console.log("---------------------------------------------------");
+                showItems();
+            }
+        })
+    })
+}
